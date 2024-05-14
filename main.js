ca2ffd513a624510164c76c8faad11d6441a48e4
n04pmint <n04.pmint@gmail.com>
2024-05-13 17:49:07 -0700

    part 2 completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* 
+// PART 1 
+
 const fs = require('fs');
 const path = require('path');
 
@@ -36,9 +39,51 @@ if (!haveBreak) {
 });
 console.log(total)
 
+*/
 
+//PART 2
+
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'input', 'input.txt')
+const input = fs.readFileSync(filePath, 'utf8')
+
+let total = 0;
+
+const lines = input.split('\r\n')
+
+lines.forEach((line, index) => {
+    console.log(`Line: ${line}`);
+    console.log(`Index: ${index + 1}`);
+    const group = line.split(': ')[1].split('; ');
+
+    let totalMap = {red: 0, green: 0, blue: 0};
+    group.forEach((g) => {
+        let inputMap = {red: 0, green: 0, blue: 0};
+        let c = []
+        g.split(', ').forEach((colors) => {
+            let splitColors = colors.split(' ');
+            let number = splitColors[0];
+            let color = splitColors[1];
+            c.push(color)
+            inputMap[color] = parseInt(number);
+            
+        })
+
+        for (index in c) {
+            let color = c[index];
+            console.log(color);
+            totalMap[color] = Math.max(totalMap[color], inputMap[color])
+        }
+        
+    })
+    total += totalMap['red'] * totalMap['green'] * totalMap['blue']
     
+})
 
+console.log(total)
 
 
 
